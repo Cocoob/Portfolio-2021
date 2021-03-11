ba1f1c5a19bdcccbe13c05a354f0d4ffd2408865
Corentin Bousquet <corentin.bousquet11@gmail.com>
2021-03-11 13:39:46 +0100

    Modif police

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const navButton = document.querySelector('.nav-button');
 const navOpen = document.querySelector('.nav-open');
+const clickImage = document.querySelector('.hide-cover-text-img');
 
 const tl = new TimelineLite({paused : true, reversed :true});
-
 
 
 tl.to('nav', 1, {
@@ -25,13 +25,14 @@ tl.to('nav', 1, {
 })
 .fromTo('.hide-cover-text',0.5, {
     opacity : 0,
+    display : 'none',
     x : 50,
     ease : Power2.easeOut
 }, {
     opacity : 1,
-    x : 0
+    x : 0,
+    display : 'block',
 })
-
 
 
 navButton.addEventListener('click',(e)=>{
@@ -47,27 +48,26 @@ navButton.addEventListener('click',(e)=>{
 });
 
 
-
 function toggleTween(tween){
     tween.reversed() ? tween.play() : tween.reverse();
 }
 
-const tlIntro = new TimelineLite({defaults:{ease: 'power1.out'}});
+// const tlIntro = new TimelineLite({defaults:{ease: 'power1.out'}});
 
-tlIntro.to('.text', {y: '0%', duration : 1.5, stagger : 0.25});
-tlIntro.to('.slider', {y : '-100%', duration : 1.5, delay : 0.5});
-tlIntro.to('.intro', { y: '-100%', duration : 1}, '-=1' );
-tlIntro.fromTo('nav', {opacity : 0}, {opacity:1, duration : 1});
-tlIntro.fromTo('.cover-date', {opacity : 0}, {opacity:1, duration : 1});
+// tlIntro.to('.text', {y: '0%', duration : 1.5, stagger : 0.25});
+// tlIntro.to('.slider', {y : '-100%', duration : 1.5, delay : 0.5});
+// tlIntro.to('.intro', { y: '-100%', duration : 1}, '-=1' );
+// tlIntro.fromTo('nav', {opacity : 0}, {opacity:1, duration : 1});
+// tlIntro.fromTo('.cover-date', {opacity : 0}, {opacity:1, duration : 1});
 
-gsap.to('.second-part', {
-    scrollTrigger : {
-    trigger : 'header',
-    pin : true,
-    start : 'top top',
-    },
-    y : - window.innerHeight,
-});
+// gsap.to('.second-part', {
+//     scrollTrigger : {
+//     trigger : 'header',
+//     pin : true,
+//     start : 'top top',
+//     },
+//     y : - window.innerHeight,
+// });
 
 
 
